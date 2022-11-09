959d56ddb54f4f99d24ab4adf49133e422e5dc68
naymur92 <naymurrahman@yahoo.com>
2022-11-09 12:58:07 +0600

    09-11

--- ang-php-mysql/ang-crud/src/app/form-validation/form-validation.component.ts
+++ ang-php-mysql/ang-crud/src/app/form-validation/form-validation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form-validation',
@@ -7,17 +8,30 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./form-validation.component.css'],
 })
 export class FormValidationComponent implements OnInit {
-  // mandoForm = new FormGroup({
-  //   name: new FormControl(),
-  //   series: new FormControl('The Mandalorian'),
-  // });
-  // onFormSubmit(): void {
-  //   console.log('Name:' + this.mandoForm.get('name').value);
-  //   console.log('Series:' + this.mandoForm.get('series').value);
-  // }
-
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 
   constructor() {}
+
+  // // Code from Book
+  // person = {
+  //   name: 'Naymur',
+  //   address: 'Chuadanga',
+  // };
+  // submitted: boolean = false;
+  // submitForm(form: NgForm) {
+  //   this.submitted = true;
+  //   if (form.valid) {
+  //     console.log('Form submitted');
+  //     this.submitted = false;
+  //   }
+  // }
 
   ngOnInit(): void {}
 }
---
+++ golden_star_beefirm/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BusinessComponent } from './business/business.component';
+import { MainNavbarComponent } from './main-navbar/main-navbar.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+import { AdminNavbarComponent } from './admin/admin-navbar/admin-navbar.component';
+import { AdminFooterComponent } from './admin/admin-footer/admin-footer.component';
+import { EmployeesComponent } from './admin/employees/employees.component';
+import { AdminComponent } from './admin/admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BusinessComponent,
+    MainNavbarComponent,
+    MainFooterComponent,
+    GalleryComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    AdminHomeComponent,
+    AdminNavbarComponent,
+    AdminFooterComponent,
+    EmployeesComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
