46094c1e0886a814c8ef319bb80e8b3cabd6c7c2
naymur92 <naymurrahman@yahoo.com>
2022-11-12 14:06:53 +0600

    12-11-home

--- golden_star_beefirm/src/app/app-routing.module.ts
+++ golden_star_beefirm/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { BusinessComponent } from './business/business.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutUsComponent},
+  {path: 'business', component: BusinessComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'contact', component: ContactUsComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
