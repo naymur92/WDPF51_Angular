f169baab929f458a418b5cd107b827ce3e7e898e
naymur92 <naymurrahman@yahoo.com>
2022-12-07 22:17:39 +0600

    golden star beefarm project moved to another repository

--- golden_star_beefirm/src/app/admin/employee.model.ts
+++
@@ -1,7 +0,0 @@
-export class Employee{
-  id?: string | undefined;
-  name?: string | undefined;
-  email?: string | undefined;
-  password?: string | undefined;
-  employee_type?: string | undefined;
-}
--- golden_star_beefirm/src/app/admin/product.model.ts
+++
@@ -1,8 +0,0 @@
-export class Product{
-  id?: string | undefined;
-  name?: string | undefined;
-  price?: string | undefined;
-  category?: string | undefined;
-  stock?: string | undefined;
-  thumbnail?: string | undefined;
-}
